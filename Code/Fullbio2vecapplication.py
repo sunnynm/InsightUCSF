90e0179ab3586c8aa13dd611f0b1d74ce42dc14e
sunnynm <snmahesh@asu.edu>
2020-01-29 16:58:26 -0800

    Create Fullbio2vecapplication.py

---
+++ Code/Fullbio2vecapplication.py
@@ -0,0 +1,78 @@
+import gensim
+import time
+start_time = time.time()
+model = gensim.models.KeyedVectors.load_word2vec_format('./BioWordVec_PubMed_MIMICIII_d200.vec.bin', binary=True)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+import pickle
+filesave = './refinedcardtopiclist.pickle'
+with open(filesave, 'rb') as fp:
+    reflist = pickle.load(fp)
+
+import pickle
+filesave = './manuallyrefinedcardtopiclist.pickle'
+with open(filesave, 'rb') as fp:
+    manreflist = pickle.load(fp)
+refleftover = list(set(reflist)-set(manreflist))
+
+from rake_nltk import Rake #https://pypi.org/project/rake-nltk/
+from nltk.corpus import stopwords
+from nltk import word_tokenize 
+from nltk.util import ngrams 
+r = Rake(stopwords = stopwords.words("english")) # Uses stopwords for english from NLTK, and all puntuation characters.Please note that "hello" is not included in the list of stopwords.
+counter = 0
+import csv
+import re
+import pandas as pd
+subdf = pd.read_csv('./AllAbstracts.csv')
+with open('./MutliKeywordDistFULL.csv', mode='w', encoding='UTF-8', newline='') as file:
+	ewrite = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	ewrite.writerow(['Document', 'Phrase', 'CardVal', 'RefinedWord', 'RefinedVal', "ManRefWord", "ManRefVal"])
+	for index, row in subdf.iterrows():
+		rtext = re.sub('[0-9]+', 'XXX', row['Abstract'])
+		a = r.extract_keywords_from_text(rtext)
+		c=r.get_ranked_phrases_with_scores()
+		for j in c:
+			if '©' in j[1]:
+				continue
+			cosinevallist = []
+			tokens = [token for token in j[1].split(" ") if token != ""]
+			for gram in range(1, 4):
+				if len(tokens) < gram:
+					break
+				ngrams = zip(*[tokens[i:] for i in range(gram)])
+				phrasetotry = ["-".join(ngram) for ngram in ngrams]
+				for phrase in phrasetotry:
+					try:
+						cval = model.similarity('cardiology', phrase)
+						cword = 'cardiology'
+					except:
+						cval = 0
+					
+					if cval != 0:
+						manrefval = cval
+						manrefword = cword
+						for reference in manreflist:
+							tempval = model.similarity(reference, phrase)
+							if tempval > manrefval:
+								manrefval = tempval
+								manrefword = reference
+						refval = manrefval
+						refword = manrefword
+						for reference in refleftover:
+							tempval = model.similarity(reference, phrase)
+							if tempval > refval:
+								refval = tempval
+								refword = reference
+						ewrite.writerow([row["Title"], phrase, cval, refword, refval, manrefword, manrefval])
+			#print([counter, j[1], j[0], mcval])
+		if (counter % 1000 == 0):
+			print("--- %s seconds ---" % (time.time() - start_time))
+			print(counter)
+		counter+=1
+print("CSV done")
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
