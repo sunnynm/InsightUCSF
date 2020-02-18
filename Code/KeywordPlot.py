f850d195c2d0267daaa31ab3649ebc84d9b6946b
sunnynm <snmahesh@asu.edu>
2020-02-18 13:24:02 -0800

    Edited Format

--- Code/KeywordPlot.py
+++ Code/KeywordPlot.py
@@ -5,7 +5,7 @@ start_time = time.time()
 model = gensim.models.KeyedVectors.load_word2vec_format('./BioWordVec_PubMed_MIMICIII_d200.vec.bin', binary=True)
 
 import pandas as pd
-df = pd.read_csv("./MutliKeywordDist.csv") 
+df = pd.read_csv("./MutliKeywordDistFULL.csv") 
 alpha = df.loc[(df['ManRefVal'] >= .66)]
 beta = df.loc[(df['ManRefVal'] >= .66) & (df['ManRefVal'] != 1)]
 keys = list(set(beta.Phrase.values))
@@ -21,6 +21,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+# modified t-SNE visulization from link below
+# link: https://towardsdatascience.com/google-news-and-leo-tolstoy-visualizing-word2vec-word-embeddings-with-t-sne-11558d8bd4d
+
 tsne_ak_2d = TSNE(perplexity=30, n_components=2, init='pca', n_iter=3500, random_state=32)
 embeddings_ak_2d = tsne_ak_2d.fit_transform(embeddings_ak)
 
@@ -34,7 +37,7 @@ def tsne_plot_2d(title, embeddings, words=[], a=1):
                      textcoords='offset points', ha='right', va='bottom', size=10)
     plt.title(title)
     plt.grid(True)
-    plt.savefig("test_relation.png", format='png', dpi=150, bbox_inches='tight')
+    plt.savefig("keyword_relations.png", format='png', dpi=150, bbox_inches='tight')
     plt.show()
 
 tsne_plot_2d('Relations Between Discovered Keywords', embeddings_ak_2d,words_ak, a= 0.7)
--- Code/RelevantDocNum.py
+++
@@ -1,9 +0,0 @@
-import time
-import datetime
-start_time = time.time()
-
-import pandas as pd
-df = pd.read_csv("./MutliKeywordDistFULL.csv") 
-alpha = df.loc[(df['ManRefVal'] >= .66)]
-print(len(set(df.Document.values)))
-print(len(set(alpha.Document.values)))
